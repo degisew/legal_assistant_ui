464686a20a38a34e1191c4d1680157418f56287c
Degisew <jegisew21@gmail.com>
2025-07-21 00:09:49 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Chat from "./components/Chat";
+import NavBar from "./components/NavBar";
+
+
+function App() {
+  return <div>
+    <NavBar />
+    <Chat />
+    </div>
+}
+
+
+export default App
