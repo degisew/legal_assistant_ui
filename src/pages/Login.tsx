99d64079317418a3e90ad944834dfd711b97c7e9
Degisew <jegisew21@gmail.com>
2025-07-23 01:10:34 +0300

    feat: created pages for routing

---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "../components/LoginForm";
+
+function Login() {
+  return (
+    <>
+      <h1>Login Page</h1>
+      <LoginForm />
+    </>
+  );
+}
+
+export default Login;
