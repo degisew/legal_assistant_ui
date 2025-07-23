21c061055287d313ed361dadf67388927ed5c3c5
Degisew <jegisew21@gmail.com>
2025-07-23 18:24:57 +0300

    feat: layout fix and styling

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,17 +1,23 @@
+import type { FormEvent } from "react";
 import "./loginForm.css";
 
 function LoginForm() {
-  const loginHandler = () => {};
+  const loginHandler = (e: FormEvent) => {
+    e.preventDefault();
+    // Add signin logic here (e.g. API call)
+    console.log("Signup submitted");
+  };
   return (
-    <>
-      <div className="login-form">
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Login</h2>
         <form onSubmit={loginHandler}>
-          <input type="text" placeholder="email" />
-          <input type="text" placeholder="password" /><br />
-          <button>Login</button>
+          <input type="email" placeholder="Email" required />
+          <input type="password" placeholder="Password" required />
+          <button type="submit">Login</button>
         </form>
       </div>
-    </>
+    </div>
   );
 }
 
