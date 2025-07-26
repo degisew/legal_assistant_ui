bf4075592ff6674f3b1e06dfebe37dd8d3a51546
Degisew <jegisew21@gmail.com>
2025-07-27 00:41:28 +0300

    feat: chat page styling and sub-component rendering

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,9 +1,39 @@
-import Chat from '../components/Chat';
+import Chat from "../components/Chat";
+import Sidebar from "../components/Sidebar";
+import "./chatpage.css";
+import { useParams } from "react-router-dom";
+import ChatWindow from "../components/ChatWindow";
+import ChatForm from "../components/ChatForm";
+import { chatLogic } from "../hooks/chatLogic";
 
 function ChatPage() {
+  const { message, selectedDoc, setMessage, handleChatSubmit } = chatLogic();
+  const { chatId } = useParams();
   return (
-    <Chat />
-  )
+    <main className="chat-page">
+      <Sidebar />
+      {!chatId ? (
+        <div className="chat-area">
+          <Chat />
+        </div>
+      ) : (
+        <div className="chat-window-container">
+          <ChatWindow
+            messages={[
+              { sender: "user", text: "hello" },
+              { sender: "bot", text: "hello! How can I help you today?" },
+            ]}
+          />
+          <ChatForm
+            message={message}
+            selectedDoc={selectedDoc}
+            handleChatSubmit={handleChatSubmit}
+            setMessage={setMessage}
+          />
+        </div>
+      )}
+    </main>
+  );
 }
 
-export default ChatPage
+export default ChatPage;
